08bf0b23e3223897ccb82d20c8fd627d303ed498
Glenn Y. Rolland <glenux@glenux.net>
2016-04-15 01:30:52 +0200

    Rename Merge to Build.

--- src/cli/jyrkkaCli.ml
+++ src/cli/jyrkkaCli.ml
@@ -1,38 +1,38 @@
 open Core.Std
 
 type cli_action_t = 
-    | CliNone
-    | CliMerge
-    | CliLoad 
+  | CliNone
+  | CliBuild
+  | CliLoad 
 
 let parse_cmdline () = 
-    let open Arg in
+  let open Arg in
 
-    (* default values *)
-    let conf_verbose = ref false in 
-    let conf_action = ref CliNone in
+  (* default values *)
+  let conf_verbose = ref false in 
+  let conf_action = ref CliNone in
 
-    let parse_action = function 
-        | "merge" -> conf_action := CliMerge
-        | "load" -> conf_action := CliLoad 
-        | _ -> conf_action := CliNone
-    in
+  let parse_action = function 
+    | "merge" -> conf_action := CliBuild
+    | "load" -> conf_action := CliLoad 
+    | _ -> conf_action := CliNone
+  in
 
-    let usage = "usage: " ^ Sys.argv.(0) ^ "[options...]" in 
-    let speclist = [
-        ("-v", Set conf_verbose,   ": set somebool to true");
-        ("-s", String parse_action, ": what follows -s sets some string")
-    ] in
-    let error_fn arg = raise (Bad ("Bad argument : " ^ arg)) in 
+  let usage = "usage: " ^ Sys.argv.(0) ^ "[options...]" in 
+  let speclist = [
+    ("-v", Set conf_verbose,   ": set somebool to true");
+    ("-s", String parse_action, ": what follows -s sets some string")
+  ] in
+  let error_fn arg = raise (Bad ("Bad argument : " ^ arg)) in 
 
-    (* Read the arguments *)
-    parse speclist error_fn usage ;
+  (* Read the arguments *)
+  parse speclist error_fn usage ;
 
-    (* Return a value *)
-    ( !conf_action, !conf_action )
+  (* Return a value *)
+  ( !conf_action, !conf_action )
 
 
 let _ = 
-    let config = parse_cmdline () in
-    print_string "Patoune"
+  let config = parse_cmdline () in
+  print_string "Patoune"
 
